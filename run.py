1b9483d5169509b9f1c0cd9c16b8faf0b5f5ab5a
Daniel Taylor <danny.taylor@live.fr>
2018-12-07 13:35:29 +0100

    chore: update main method with command line args

--- run.py
+++ run.py
@@ -1,8 +1,39 @@
 # bin/python3
-
+import sys
+import getopt
 from cacm_part1.DocumentParser import DocumentParser
 
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h f", ["help", "file"])
+    except getopt.GetoptError:
+        print("For help, type: --help")
+        sys.exit(2)
+    
+    HELP = """
+        options:
+            -h --help : shows this help message
+            -f --file : the path to the input file
+    """
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print(HELP)
+            sys.exit(0)
+        if o in ("-f", "--file"):
+            i = opts[0].index(o)
+            if (len(args) == 0):
+                print("Please enter a filename")
+                sys.exit(1)
+            filename = args[i]
+            doc_list = DocumentParser.parse_entry_file(filename)
+            tokens = DocumentParser.tokenize(doc_list)
+            print(tokens)
+            sys.exit(0)
+        print("Please enter a file name -f or --file")
+        sys.exit(2)
+        
+
 if __name__ == '__main__':
-    doc_list = DocumentParser.parse_entry_file("cacm/cacm.all")
-    tokens = DocumentParser.tokenize(doc_list)
-    print(tokens)
+    sys.exit(main())
