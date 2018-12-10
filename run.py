2a32bde62973ec920d3f1b12637a20e8af1635f2
Daniel Taylor <danny.taylor@live.fr>
2018-12-10 11:53:32 +0100

    caculate heap law tokens

--- run.py
+++ run.py
@@ -2,8 +2,47 @@
 import sys
 import getopt
 import gc
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 from cacm_part1.DocumentParser import DocumentParser
 
+
+
+def estimate_coef(x, y): 
+    # number of observations/points 
+    n = np.size(x) 
+  
+    # mean of x and y vector 
+    m_x, m_y = np.mean(x), np.mean(y) 
+  
+    # calculating cross-deviation and deviation about x 
+    SS_xy = np.sum(y*x) - n*m_y*m_x 
+    SS_xx = np.sum(x*x) - n*m_x*m_x 
+  
+    # calculating regression coefficients 
+    b_1 = SS_xy / SS_xx 
+    b_0 = m_y - b_1*m_x 
+  
+    return(b_0, b_1) 
+
+def plot_regression_line(x, y, b): 
+    # plotting the actual points as scatter plot 
+    plt.scatter(x, y, color = "m", 
+               marker = "o", s = 30) 
+  
+    # predicted response vector 
+    y_pred = b[0] + b[1]*x 
+  
+    # plotting the regression line 
+    plt.plot(x, y_pred, color = "g") 
+  
+    # putting labels 
+    plt.xlabel('x') 
+    plt.ylabel('y') 
+  
+    # function to show plot 
+    plt.show()
 
 def main():
     try:
@@ -39,10 +78,26 @@ def main():
     if filename is None:
         print("Please provide a filename")
     doc_list = DocumentParser.parse_entry_file(filename)
-    tokens = DocumentParser.tokenize(doc_list, "cacm/common_words")
-    vocab = DocumentParser.calculate_vocabulary(tokens)
-    print(vocab)
+    tokens, token_counts = DocumentParser.tokenize(doc_list, "cacm/common_words")
+    vocab, vocab_lengths = DocumentParser.calculate_vocabulary(tokens)
+    for i in range(len(token_counts)):
+        token_counts[i] = math.log10(token_counts[i])
+    for i in range(len(vocab_lengths)):
+        vocab_lengths[i] = math.log10(vocab_lengths[i])
+    token_counts = np.asarray(token_counts)
+    vocab_lengths = np.asarray(vocab_lengths)
+    coefs = estimate_coef(token_counts, vocab_lengths)
+    print(coefs)
+    k = 10**coefs[0]
+    b = coefs[1]
+    print("k= " + str(10**coefs[0]) + " b= " + str(coefs[1]))
+    voc_million = k * (10**6)**b
+    print("Vocabulaire 1million tokens: " + str(voc_million))
+    plot_regression_line(token_counts, vocab_lengths, coefs)
     tokens = None
+    token_counts = None
+    vocab_lengths = None
+    vocab = None
     gc.collect()
     sys.exit(0)
         
